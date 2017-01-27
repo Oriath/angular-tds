6fca848adbc457b2300cff11a3c86c50089635b2
Guillaume Picard <21501318@etu.unicaen.fr>
2017-01-27 12:05:46 +0100

    Ajout des fonctionnalités 1 à 3 (pas fini)
    Ajout des variables, fonctions et code html nécesaires.
    Reste fonctionnalités à terminer.

--- js/noteController.js
+++ js/noteController.js
@@ -2,5 +2,27 @@
  * Created by Utilisateur on 27/01/2017.
  */
 angular.module("NoteApp").controller("NoteController", function(){
+    this.messageNote = "";
+    this.info = "";
+    this.status = 1;
 
+    this.save = function(){
+        this.status = 0;
+        this.info = "Note sauvegardée";
+    };
+
+    this.clear = function(){
+        this.messageNote = "";
+    };
+
+    this.count = function(){
+        var cpt = 100 - this.messageNote.length;
+        if(cpt < 20)
+            this.status = 3;
+        else if(cpt < 50)
+            this.status = 2;
+        else
+            this.status = 1;
+        return cpt;
+    };
 });
