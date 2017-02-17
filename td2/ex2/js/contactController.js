19beddc56eb89f26a1e0f6d951e1d47e5de72112
Guillaume Picard <21501318@etu.unicaen.fr>
2017-02-17 10:47:19 +0100

    Abondon ex1 début ex2
    Exercice problèmes d'ajout qui ne fait rien sans erreur dans la console,
    même avec du code pris sur d'autres pour qui ça marche, ne comprends
    pas.
    Début du nouvel exercice

---
+++ td2/ex2/js/contactController.js
@@ -0,0 +1,30 @@
+app.controller("ListesController", function() {
+
+    var self = this;
+
+    this.contacts = [];
+    this.contact = [];
+    this.tmpContact = [];
+    this.operation = "";
+    this.edit = "";
+
+    this.toUpdate = function(contact){
+
+    };
+
+    this.toAdd = function(){
+
+    };
+
+    this.add = function(){
+
+    };
+
+    this.update = function(){
+
+    };
+
+    this.delete = function(contact){
+
+    };
+});
