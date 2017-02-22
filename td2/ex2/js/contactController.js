044a2cd198a364656849ab9b0c55f83cf343f0d0
Guillaume Picard <21501318@etu.unicaen.fr>
2017-02-22 11:13:30 +0100

    Ajout formulaire + fonctions js
    Le formulaire est créé, on peut l'afficher en fonction de edit, mais les
    boutons de modif et suppression ne marchent pas à cause du scope de la
    directive alors que chez les autres cela marche, raison inconnue. Les
    fonctions js ne marchent pas non plus comme voulu, à revoir

--- td2/ex2/js/contactController.js
+++ td2/ex2/js/contactController.js
@@ -13,28 +13,40 @@ angular.module("ContactApp").controller("ContactController", function() {
             "prenom":"Guillaume",
             "mail":"yoyoyo@gmail.com"}
     ];
-    this.contact = [];
-    this.tmpContact = [];
+    this.contact = {};
+    this.tmpContact = {};
     this.operation = "";
-    this.edit = "";
+    this.edit = false;
 
     this.toUpdate = function(contact){
-
+        self.edit = true;
+        self.contact = contact;
+        self.operation = "Modifier un contact";
     };
 
     this.toAdd = function(){
-
+        self.edit = true;
+        self.operation = "Ajouter un contact";
     };
 
     this.add = function(){
-
+        self.edit = false;
+        self.tmpContact = {"nom":"Picard",
+            "prenom":"Guillaume",
+            "mail":"yoyoyo@gmail.com"};
+        self.contacts.push(self.tmpContact);
+        console.log(self.tmpContact);
     };
 
     this.update = function(){
-
+        self.edit = false;
+        self.tmpContact = {"nom":"Picard",
+            "prenom":"Guillaume",
+            "mail":"yoyoyo@gmail.com"};
+        self.contacts.splice(indexOf(self.contact), 1, self.tmpContact);
     };
 
     this.delete = function(contact){
-
+        self.contacts.splice(contacts.indexOf(contact), 1);
     };
 });
--- td2/ex2/js/directives.js
+++ td2/ex2/js/directives.js
@@ -1,7 +1,24 @@
-angular.module("ContactApp").directive("contactElem", function(){
+angular.module("ContactApp").directive("contactElem",function(){
     return{
         restrict:'EA',
-        template:'<td>{{contact.nom}}</td><td>{{contact.prenom}}</td><td>{{contact.mail}}</td><td><div class="btn btn-info btn-xs glyphicon glyphicon-edit" data-ng-click="toUpdate(contact)" aria-hidden="true"></div> <div class="btn btn-danger btn-xs glyphicon glyphicon-remove-circle" data-ng-click="delete(contact)" aria-hidden="true"></div></td>',
+        template:'<td>{{contact.nom}}</td>' +
+                 '<td>{{contact.prenom}}</td>' +
+                 '<td>{{contact.adresse}}</td>' +
+                 '<td><a class="btn btn-info btn-xs" href="" data-ng-click="cttCtrl.toUpdate(contact)"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>' +
+                 '&nbsp;<a class="btn btn-danger btn-xs" href="" data-ng-click="cttCtrl.delete(contact)"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a></td>',
         scope:{contact:'='}
+    }
+});
+
+angular.module("ContactApp").directive("frmContact", function(){
+    return{
+        restrict:'EA',
+        template:'<form name="frmContact" data-ng-submit="frmContact.$valid &amp;&amp; cttCtrl.update()" class="ng-pristine ng-invalid ng-invalid-required ng-valid-email">' +
+                 '<input class="form-control ng-pristine ng-untouched ng-invalid ng-invalid-required" type="text" data-ng-model="cttCtrl.contact.nom" placeholder="Entrez un nom..." required="true">' +
+                 '<input class="form-control ng-pristine ng-untouched ng-valid" type="text" data-ng-model="cttCtrl.contact.prenom" placeholder="Prénom...">' +
+                 '<input class="form-control ng-pristine ng-valid-email ng-invalid ng-invalid-required ng-touched" type="email" data-ng-model="CttCtrl.contact.email" placeholder="Adresse email...">' +
+                 '<input type="submit" value="Valider" class="btn btn-primary">' +
+                 '<input type="button" value="Annuler" data-ng-click="cttCtrl.edit=false" class="btn btn-primary">' +
+                 '</form>'
     };
 });
