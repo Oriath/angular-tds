81ffbf5c42d337bae0c5919138030a47617494a7
Guillaume Picard <21501318@etu.unicaen.fr>
2017-03-10 11:00:42 +0100

    Fin du currency + début de la calculatrice
    Fin de l'exercice 1 sur la conversion (historique)
    Création des fichiers pour l'exercice sur la calculatrice

--- td3/ex1/js/currencyController.js
+++ td3/ex1/js/currencyController.js
@@ -3,51 +3,48 @@ app.controller("CurrencyController", ['$http', function($http) {
     var self = this;
 
     this.currencies = {};
+    this.from = "";
+    this.to = "";
 
     $http.get('data/currencymap.json').then(function(response) {
             self.currencies = response.data;
+            self.from = self.currencies["EUR"];
+            self.to = self.currencies["USD"];
         },
         function(response) {
             console.log("Erreur avec le statut Http : "+response.status);
         });
 
-    this.from = "EUR";
-    this.to = "USD";
     this.what = 1;
     this.result;
     this.histo = true;
-    this.historique = [
-        {
-            id : 1,
-            conversion : {from : 1,
-                to : 1,
-                amount : function(){return 1;},
-                initialAmount : function(){return 0;},
-                delta : 1,
-                rate : 1,
-                what : 1,
-                date : 1,
-                update: "ok",
-                initialRate : 1
-            }
-        }
-    ];
-    this.conversion={from : this.from,
-        to : this.to,
-        amount : function(){return 1;},
-        initialAmount : function(){return 0;},
-        delta : 1,
-        rate : 1,
-        what : this.what,
-        date : "",
-        update: "ok",
-        initialRate : 1
-    };
+    this.historique = {};
 
     this.getResult = function(){
-        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from+'_'+self.to, {jsonpCallbackParam: 'callback'})
+        $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from.code+'_'+self.to.code, {jsonpCallbackParam: 'callback'})
             .then(function(response) {
-                self.result=response.data[self.from+'_'+self.to].val;
+                self.result = response.data[self.from.code + '_' + self.to.code].val * self.what;
+                var conversion = {
+                    from : self.from,
+                    to : self.to,
+                    amount : function(){ return self.what * this.rate},
+                    initialAmount : function(){ return self.what * this.initialRate},
+                    delta : 0,
+                    rate : response.data[self.from.code + '_' + self.to.code].val,
+                    what : self.what,
+                    date : new Date(),
+                    update: false,
+                    initialRate : response.data[self.from.code + '_' + self.to.code].val
+                };
+                var key = self.from.code + self.to.code;
+                if(self.historique[key]){
+                    var oldConversion = self.historique[key];
+                    oldConversion.what = self.what;
+                    conversion.delta = conversion.amount() - oldConversion.initialAmount();
+                    conversion.initialRate = oldConversion.initialRate;
+                }
+                conversion.update = false;
+                self.historique[key] = conversion;
             });
     };
 
